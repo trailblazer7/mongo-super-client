a3bba32147295f760c39046d4ad2c12ee360c67a
trailblazer <antonmatviets@gmail.com>
2017-06-16 10:03:10 +0300

    karma config

--- tests/unit-ui/karma.conf.js
+++ tests/unit-ui/karma.conf.js
@@ -13,7 +13,7 @@ module.exports = config => {
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
-    browsers: ['Electron'],
+    browsers: ['Chrome', 'Firefox'],
     // If browser does not capture in given timeout [ms], kill it
     captureTimeout: 60000,
     // // Continuous Integration mode
